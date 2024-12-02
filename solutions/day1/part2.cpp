8284712eb1c3e593e17e6b30aa0e7426dc730bd9
Daniel Dron <danieldron2@gmail.com>
2024-12-02 18:21:09 +0000

    Change file structure

---
+++ solutions/day1/part2.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <charconv>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <ranges>
+#include <string>
+#include <vector>
+
+int main() {
+  std::ifstream file("day1.txt");
+  file.tie(nullptr);
+  std::ios::sync_with_stdio(false);
+
+  std::vector<int> left;
+  std::vector<int> right;
+
+  std::string line;
+  line.reserve(5); // 3...5
+
+  while (std::getline(file, line)) {
+    int l, r = {};
+    std::string_view sv = line;
+    auto pos = sv.find_first_of(" ");
+    std::from_chars(sv.data(), sv.data() + pos, l);
+    auto pos2 = sv.find_first_not_of(" ", pos);
+    std::from_chars(sv.data() + pos2, sv.data() + sv.size(), r);
+
+    left.emplace_back(l);
+    right.emplace_back(r);
+  }
+  
+  std::sort(left.begin(), left.end());
+  std::sort(right.begin(), right.end());
+
+  int sum = {};
+
+  int last_l = 0;
+  int last_s = 0;
+  for (auto l : left) {
+    if (l == last_l) {
+        sum += last_s;
+    }
+
+    last_l = l;
+  
+    int count = std::ranges::count(right, l);
+    last_s = l * count;
+
+    sum += last_s;
+  }
+
+  std::printf("answer: %d\n", sum);
+}
